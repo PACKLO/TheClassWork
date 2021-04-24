ccc407a54b6155b667b3f25d2f02a3d96cc290d8
nopeitagain@mail.ru <nopeitagain@mail.ru>
2021-04-24 18:32:10 +0300

    Нахождение корня линейного и квадратного уравнения.

--- PCW/PCW.cpp
+++ PCW/PCW.cpp
@@ -2,28 +2,27 @@
 #include <stdlib.h>
 #include <time.h>
 using namespace std;
-int massive[1];
-int massivemassivity = 0, allmassive = 0, result = 0;
-void MidArif(int Number1, int Number2)
+int a, x, b, c;
+void LinUr()
 {
-	int massiveraz = Number2 - Number1;
-	for (int i = Number1; i <= Number2; i++)
-	{
-		massive[i] = i;
-		massivemassivity += 1;
-		allmassive += i;
-	}
-	result = allmassive / massivemassivity;
-	cout <<"Результат: " << result;
+	x = -b / a;
+	cout << x << endl;
+}
+void KLinUr()
+{
+	x = -(c / b);
+	cout << x << endl;
 }
 
 int main()
 {
-	int n1, n2;
 	setlocale(LC_ALL, "Russian");
-	cout << "Начало: ";
-	cin >> n1;
-	cout << "Конец: ";
-	cin >> n2;
-	MidArif(n1, n2);
+	cout << "A: ";
+	cin >> a;
+	cout << "B: ";
+	cin >> b;
+	cout << "C: ";
+	cin >> c;
+	LinUr();
+	KLinUr();
 }
